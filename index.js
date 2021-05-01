2533d40e3e4c752bbc39eebd19e96b79f9a1f1b9
syedeasin <syedeasin1000@gmail.com>
2021-05-01 13:24:53 +0600

    1st Testimonial

---
+++ index.js
@@ -0,0 +1,40 @@
+//getElementByID
+// document.getElementById("heading1")..innerHTML = "It's a DOM Methode";
+// var heading = document.getElementById("heading1");
+// heading.innerHTML = "It's a DOM Methode";
+
+// var paragra = document.getElementById("para");
+// paragra.innerHTML = "vjhnaejkkbnja afdojvna voioanfv avhna fva;oi afjvnak ";
+// document.getElementsByTagName("h1")[2].innerHTML = "Easin: About Me";
+// document.getElementsByClassName("heading")[1].innerHTML = "Ok, It's Me Easin";
+// document.querySelector("#heading3").innerHTML = "Ok, It's Me Easin";
+// document.querySelector(".heading").innerHTML = "My Name Is Syed Vai";
+// document.querySelectorAll("h1")[0].innerHTML = "Finally querySelector is Done:: Updated";
+
+// ------ Event Handler ------
+
+function text1()
+{
+    var comments = document.getElementById("comment");
+    comments.innerHTML = '"Spread love everywhere you go. Let no one ever come to you without leaving happier."';
+}
+function text2()
+{
+    var comments = document.getElementById("comment");
+    comments.innerHTML = '"When you reach the end of your rope, tie a knot in it and hang on."';
+}
+function text3()
+{
+    var comments = document.getElementById("comment");
+    comments.innerHTML = '"Always remember that you are absolutely unique. Just like everyone else."';
+}
+function text4()
+{
+    var comments = document.getElementById("comment");
+    comments.innerHTML = '"Dont judge each day by the harvest you reap but by the seeds that you plant."';
+}
+function text5()
+{
+    var comments = document.getElementById("comment");
+    comments.innerHTML = '"The future belongs to those who believe in the beauty of their dreams."';
+}
